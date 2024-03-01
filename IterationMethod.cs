8e932497d0587e24725cd7da15a3915b6f549117
Petro Horokh <petrohorokh@gmail.com>
2024-03-01 23:59:14 +0200

    Add project files.

---
+++ IterationMethod.cs
@@ -0,0 +1,121 @@
+﻿namespace Lab2;
+
+public static class IterationMethod
+{
+    public static void IterationFunction(double[,] A, double[] f, double e)
+    {
+        double[] x = [0, 0, 0, 0, 0];
+        int counter = 0;
+        int n = x.Length;
+        double norm;
+        double[] multiplication, multiplicationJacobi, prepF;
+        double[] priorX = new double[n];
+        do
+        {
+            norm = 0;
+
+            Array.Copy(x, priorX, n);
+
+            multiplicationJacobi = MultiplyJacoby(A, priorX);
+            prepF = PrepareFunctionVector(A, f);
+
+            x = Subtraction(prepF, multiplicationJacobi);
+
+            for (int i = 0; i < n; i++)
+            {
+                norm += Math.Pow(x[i] - priorX[i], 2);
+            }
+
+            norm = Math.Sqrt(norm);
+
+            counter++;
+
+            multiplication = Multiply(A, prepF);
+
+        } while (norm >= e);
+
+        var incoherency = Subtraction(multiplication, f);
+
+        Console.WriteLine($"Accuracy: {e}");
+        Console.WriteLine($"Iteration quantity: {counter}");
+        Console.WriteLine("\nResult:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"х[{(i + 1)}] = {x[i]}");
+        }
+
+        Console.WriteLine("\nIncoherency vector:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"r[{(i + 1)}] = {incoherency[i]}");
+        }
+    }
+    public static double[] MultiplyJacoby(double[,] matrixA, double[] matrixB)
+    {
+        double[] result = new double[matrixA.GetLength(0)];
+        int n = matrixA.GetLength(0);
+
+        for (int i = 0; i < n; i++)
+        {
+            double sum = 0;
+
+            var a = matrixA[i, i];
+
+            for (int j = 0; j < n; j++)
+            {
+                sum += matrixA[i, j] / a * matrixB[j];
+            }
+
+            sum -= matrixB[i];
+
+            result[i] = sum;
+        }
+
+        return result;
+    }
+    public static double[] PrepareFunctionVector(double[,] matrix, double[] f)
+    {
+        int n = matrix.GetLength(0);
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = f[i] / matrix[i, i];
+        }
+
+        return result;
+    }
+    public static double[] Multiply(double[,] matrixA, double[] matrixB)
+    {
+        int n = matrixA.GetLength(0);
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            double sum = 0;
+
+            for (int j = 0; j < n; j++)
+            {
+                sum += matrixA[i, j] * matrixB[j];
+            }
+
+            result[i] = sum;
+        }
+
+        return result;
+    }
+    public static double[] Subtraction(double[] arrayA, double[] arrayB)
+    {
+        int n = arrayA.Length;
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = arrayA[i] - arrayB[i];
+        }
+
+        return result;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Lab2;
+
+Console.Write("Enter power of precision: ");
+var power = Console.ReadLine();
+
+double[,] A =
+{
+    {.38, -.05, .01, .02, .07 },
+    {.052, .595, 0, -.04, .04 },
+    {.03, 0, .478, -.14 , .08 },
+    {-.06, .126, 0, .47, -.02},
+    {.25, 0, .09, .01, .56}
+};
+double[] f = [2.32, 2.544, -3.238, 1.534, 0.12];
+var e = Convert.ToDouble($"1E-{power}");
+IterationMethod.IterationFunction(A, f, e);
